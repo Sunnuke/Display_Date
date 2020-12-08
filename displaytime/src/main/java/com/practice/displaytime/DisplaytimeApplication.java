152174bbc3f8ce4c7bb2f814a98ca956cabdead9
Javier Rivera <Sunnuke@users.noreply.github.com>
2020-12-07 22:15:05 -0800

    Created DisplaytimeApplication class to manage the routes for displaying the user's local time and date on templates

--- displaytime/src/main/java/com/practice/displaytime/DisplaytimeApplication.java
+++ displaytime/src/main/java/com/practice/displaytime/DisplaytimeApplication.java
@@ -1,13 +1,41 @@
 package com.practice.displaytime;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
+@Controller
 public class DisplaytimeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DisplaytimeApplication.class, args);
+	}
+	
+	@RequestMapping("/")
+	public String home() {
+		return "index.jsp";
+	}
+	
+	@RequestMapping("/date")
+	public String date(Model model) {
+		LocalDate date = LocalDate.now();
+		String currDate = "" + date;
+		model.addAttribute("date", currDate);
+		return "date.jsp";
+	}
+	
+	@RequestMapping("/time")
+	public String time(Model model) {
+		LocalTime date = LocalTime.now();
+		String currDate = "" + date;
+		model.addAttribute("date", currDate);
+		return "time.jsp";
 	}
 
 }
